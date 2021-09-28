e22f5eff8e7d186608427ba3cc2210eb3e13d641
Mark Rawls <markrawls96@gmail.com>
2021-09-28 18:23:26 -0400

    Default values for fields

--- sheepobot/db/command.py
+++ sheepobot/db/command.py
@@ -1,12 +1,14 @@
 from .base import BaseModel
 from peewee import TextField, IntegerField
 
+from sheepobot.enums import PermissionsLevel
+
 
 class Command(BaseModel):
     name = TextField(index=True, unique=True)
     output = TextField()
-    permissions_level = IntegerField()
-    called = IntegerField()
+    permissions_level = IntegerField(default=PermissionsLevel.USER)
+    called = IntegerField(default=0)
 
     def call(self):
         self.called += 1
